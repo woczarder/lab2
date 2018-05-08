132d8cd797eb6c3e10ebc77750684ebce9ed9d0b
woczarder <wojtasessek1997@gmail.com>
2018-05-08 19:23:08 +0200

    Zadania zrobione. Teraz na ocene

--- Main.java
+++ Main.java
@@ -1,16 +1,42 @@
 package dev.hdstudio.wat.lab2;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-	Names names = new Names();
-	names.choose();
+        FrequentNames names = new FrequentNames();
+        names.insert("Tomasz");
+        names.insert("Tomasz");
+        names.insert("Rafal");
+        names.insert("Rafal");
+        names.insert("Rafal");
+        names.insert("Wojtek");
+        names.insert("Zuza");
+        names.insert("Zuza");
+
+        names.choose();
+
+
+        Set set = names.frequentNames.entrySet();
+
+        // Get an iterator
+        Iterator i = set.iterator();
+
+        // Display elements
+        while(i.hasNext()) {
+            Map.Entry me = (Map.Entry)i.next();
+            System.out.print(me.getKey() + ": ");
+            System.out.println(me.getValue());
+        }
+
     }
 }
 
 /*
-A List
-B Set
-C Map
-D Queue
+W końcu firma ta zdecydowała się użyć tylko najczęściej występujących imion swoich
+pracowników do nazwania produktów. Policzono ilu pracowników posiada dane imię. Trzeba
+przygotować listę imion. Map
  */
